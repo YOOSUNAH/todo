f4202cfea67c2a41a4c3f61ad8ac27c2115a641b
YOOSUNAH <tjsdkyd4027@gmail.com>
2024-02-03 15:18:52 +0900

    회원가입 제약 조건 추가 (validation)활용

--- src/main/java/com/sparta/todo/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/todo/dto/SignupRequestDto.java
@@ -1,17 +1,23 @@
 package com.sparta.todo.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class SignupRequestDto {
+
     @NotBlank(message = "이름을 입력해주세요.")
+    @Size(min=4, max=10)
+    @Pattern(regexp = "^[a-z0-9]+$")
     private String username;
 
     @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Size(min=8, max=15)
+    @Pattern(regexp = "^[a-zA-Z0-9]+$")
     private String password;
-
 
 }
---
+++ src/main/java/com/sparta/todo/dto/SignupResponseDto.java
@@ -0,0 +1,18 @@
+package com.sparta.todo.dto;
+
+import com.sparta.todo.entity.User;
+import lombok.Getter;
+
+@Getter
+public class SignupResponseDto {
+    private Long userId;
+    private String username;
+    private String password;
+
+
+    public SignupResponseDto(User user){
+        this.userId = user.getUserId();
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+    }
+}

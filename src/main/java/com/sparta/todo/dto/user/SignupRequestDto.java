5d45fe771c443a2d2c0244c20b51c17d4a51cfeb
YOOSUNAH <tjsdkyd4027@gmail.com>
2024-02-04 17:14:15 +0900

    입력메세지 추가 설명

--- src/main/java/com/sparta/todo/dto/user/SignupRequestDto.java
+++ src/main/java/com/sparta/todo/dto/user/SignupRequestDto.java
@@ -10,12 +10,12 @@ import lombok.Setter;
 @Setter
 public class SignupRequestDto {
 
-    @NotBlank(message = "이름을 입력해주세요.")
+    @NotBlank(message = "이름을 입력해주세요. (4글자 이상 10글자 이하로)")
     @Size(min=4, max=10)
     @Pattern(regexp = "^[a-z0-9]+$")
     private String username;
 
-    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @NotBlank(message = "비밀번호를 입력해주세요. (8글자 이상 15글자 이하로)")
     @Size(min=8, max=15)
     @Pattern(regexp = "^[a-zA-Z0-9]+$")
     private String password;

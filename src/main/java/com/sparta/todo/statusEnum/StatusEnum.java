1acc066605eb58b618a8a867c6fc8ae82d7ecfb7
YOOSUNAH <tjsdkyd4027@gmail.com>
2024-02-04 17:03:57 +0900

    결과 메세지와 상태코드 알려주기 위한 enum에서 사용하지 않는 enum 삭제

--- src/main/java/com/sparta/todo/statusEnum/StatusEnum.java
+++ src/main/java/com/sparta/todo/statusEnum/StatusEnum.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public enum StatusEnum {
     SIGNUP_SUCCESS("회원 가입에 성공했습니다.",200),
-    LOGIN_SUCCESS( "로그인에 성공했습니다.", 200);
+    LOGIN_SUCCESS( "로그인에 성공했습니다. (토큰을 생성 완료)", 200);
 
     String message;
     Integer statusCode;

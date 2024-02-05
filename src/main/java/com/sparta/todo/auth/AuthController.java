d04843940c0640e286993b51303d1a22b08d0b89
YOOSUNAH <tjsdkyd4027@gmail.com>
2024-02-05 20:20:42 +0900

    사용하지 않는 것 같은 부분 주석처리

---
+++ src/main/java/com/sparta/todo/auth/AuthController.java
@@ -0,0 +1,75 @@
+package com.sparta.todo.auth;
+
+import com.sparta.todo.jwt.JwtUtil;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class AuthController {
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    private final JwtUtil jwtUtil;
+
+
+    @GetMapping("/create-session")
+    public String createSession(HttpServletRequest req) {
+        // 세션이 존재할 경우 세션 반환, 없을 경우 새로운 세션을 생성한 후 반환
+        HttpSession session = req.getSession(true);
+
+        // 세션에 저장될 정보 Name - Value 를 추가합니다.
+        session.setAttribute(AUTHORIZATION_HEADER, "Robbie Auth");
+
+        return "createSession";
+    }
+    @GetMapping("/get-session")
+    public String getSession(HttpServletRequest req) {
+        // 세션이 존재할 경우 세션 반환, 없을 경우 null 반환
+        HttpSession session = req.getSession(false);
+
+        String value = (String) session.getAttribute(AUTHORIZATION_HEADER); // 가져온 세션에 저장된 Value 를 Name 을 사용하여 가져옵니다.
+        System.out.println("value = " + value);
+
+        return "getSession : " + value;
+    }
+
+//    @GetMapping("/create-jwt")
+//    public String createJwt(HttpServletResponse res) {
+//        // Jwt 생성
+//        String token = jwtUtil.createToken();
+//
+//        // Jwt  저장
+//
+//        return "createJwt : " + token;
+//    }
+
+    @GetMapping("/get-jwt")
+    public String getJwt(@CookieValue(JwtUtil.AUTHORIZATION_HEADER) String tokenValue) {
+        // JWT 토큰 substring
+        String token = jwtUtil.substringToken(tokenValue);
+
+        // 토큰 검증
+        if(jwtUtil.validateToken(token)){  // validateToken 검증하는 메서드, 문제가 없으면 true로 나옴
+            throw new IllegalArgumentException("Token Error");
+        }
+
+        // 토큰에서 사용자 정보 가져오기
+        Claims info = jwtUtil.getUserInfoFromToken(token);
+        // 사용자 username
+        String username = info.getSubject();
+        Long user_id = Long.valueOf(info.getId());
+        System.out.println("username = " + username + "user_id = " + user_id);
+        return "getJwt : " + username + "user_id : " + user_id ;
+    }
+
+
+}

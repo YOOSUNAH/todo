b71f03616ac7a782e383140ad609732cd770030d
YOOSUNAH <tjsdkyd4027@gmail.com>
2024-02-05 20:23:38 +0900

    enum명 변경

--- src/main/java/com/sparta/todo/statusEnum/StatusAndEnum.java
+++ src/main/java/com/sparta/todo/statusEnum/StatusAndEnum.java
@@ -5,13 +5,13 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-public enum StatusEnum {
+public enum StatusAndEnum {
     SIGNUP_SUCCESS("회원 가입에 성공했습니다.",200),
     LOGIN_SUCCESS( "로그인에 성공했습니다. (토큰을 생성 완료)", 200);
 
     String message;
     Integer statusCode;
-    StatusEnum(String message, Integer statusCode) {
+    StatusAndEnum(String message, Integer statusCode) {
         this.message = message;
         this.statusCode = statusCode;
     }

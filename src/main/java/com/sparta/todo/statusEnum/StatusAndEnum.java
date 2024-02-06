d0acc459ff5804db2f88bdc09eeb73d425c76ea5
YOOSUNAH <tjsdkyd4027@gmail.com>
2024-02-06 16:52:31 +0900

    예외처리를 위한 enum 생성

--- src/main/java/com/sparta/todo/statusEnum/StatusAndEnum.java
+++ src/main/java/com/sparta/todo/statusEnum/StatusAndEnum.java
@@ -7,7 +7,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public enum StatusAndEnum {
     SIGNUP_SUCCESS("회원 가입에 성공했습니다.",200),
-    LOGIN_SUCCESS( "로그인에 성공했습니다. (토큰을 생성 완료)", 200);
+    LOGIN_SUCCESS( "로그인에 성공했습니다. (토큰을 생성 완료)", 200),
+    DUPLICATED_USERNAME("중복된 username 입니다.", 400),
+
+
+    INVALID_TOKEN("토큰이 유효하지 않습니다.", 400),
+    NOT_RIGHT_USER("작성자만 삭제/수정할 수 있습니다.", 400),
+    NOT_FOUND_USER("회원을 찾을 수 없습니다.", 400);
+
 
     String message;
     Integer statusCode;

1d55dcafe86a7af1b32783a03d5534d391d23922
YOOSUNAH <tjsdkyd4027@gmail.com>
2024-02-08 11:18:22 +0900

    :recycle:  : 중복되는 url RequestMapping에 공통으로 기재(feedback 반영)

--- src/main/java/com/sparta/todo/controller/TodoController.java
+++ src/main/java/com/sparta/todo/controller/TodoController.java
@@ -15,27 +15,27 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/todos")
 public class TodoController {
 
     private final TodoService todoService;
 
     // 일정 추가
-    @PostMapping("/todos")
+    @PostMapping("")
     @Operation(summary = "일정 추가", description = "제목, 내용을 입력해주세요")
     public TodoResponseDto saveTodo(HttpServletRequest request, @RequestBody TodoRequestDto todoRequestDto) {
         return todoService.saveTodo(request, todoRequestDto);
     }
 
     // 일정 목록 조회
-    @GetMapping("/todos")
+    @GetMapping("")
     @Operation(summary = "일정 목록 조회", description = "전체 일정 목록을 조회해줍니다")
     public List<TodoListResponseDto> getTodos() {
         return todoService.getTodos();
     }
 
     // 선택 일정 조회
-    @GetMapping("/todos/{todoId}")
+    @GetMapping("/{todoId}")
     @Operation( summary = "선택 일정 조회",  description = "조회하고자 하는 일정의 아이디를 입력해주세요")
     public TodoResponseDto getTodoById(@PathVariable Long todoId) {
         return todoService.getTodoById(todoId);
@@ -43,14 +43,14 @@ public class TodoController {
 
 
     // 선택 일정 수정
-    @PutMapping("/todos/{todoId}")
+    @PutMapping("/{todoId}")
     @Operation(summary = "선택 일정 수정", description = "수정하고자 하는 일정의 아이디를 입력해주세요")
     public TodoResponseDto updateTodo(HttpServletRequest request, @PathVariable Long todoId, @RequestBody TodoRequestDto todoRequestDto, Boolean isCompleted) {
         return todoService.updateTodo(request, todoId, todoRequestDto, isCompleted);
     }
 
     // 선택 일정 삭제
-    @DeleteMapping("/todos/{todoId}")
+    @DeleteMapping("/{todoId}")
     @Operation(summary = "선택 일정 삭제", description = "삭제하고자 하는 일정의 아이디를 입력해주세요")
     public Long deleteTodo(@PathVariable Long todoId) {
         return todoService.deleteTodo(todoId);

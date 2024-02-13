22be6874dd64da9a037233cc69a870747323313d
YOOSUNAH <tjsdkyd4027@gmail.com>
2024-02-13 16:06:54 +0900

    :adhesive_bandage:    : comment Api 접근 허용을 위해 WebSecurityConfig 변경

--- src/main/java/com/sparta/todo/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/todo/config/WebSecurityConfig.java
@@ -40,8 +40,7 @@ public class WebSecurityConfig implements WebMvcConfigurer {
             authorizeHttpRequests
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
                 .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll() // swagger 접근 허용 설정
-                .requestMatchers("/api/v1/user/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
-                .requestMatchers("/api/v1/todos/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
+                .requestMatchers("/api/v1/**").permitAll() // '/api/v1/'로 시작하는 요청 모두 접근 허가
                 .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
 

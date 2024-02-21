d4b567d62457c93776f29aa85a71fbc48410466e
YOOSUNAH <tjsdkyd4027@gmail.com>
2024-02-21 14:22:24 +0900

    :sparkles: Todo entity의 complete 메서드 테스트

---
+++ src/test/java/com/sparta/todo/entity/TodoTest.java
@@ -0,0 +1,27 @@
+package com.sparta.todo.entity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class TodoTest {
+
+    @Test
+    @DisplayName("완료 여부 확인")
+    void complete() {
+        // given
+         //build 또는 tool (reflection test util(dto에 작업하지 않아도 된다.)
+        Todo todo = Todo.builder()
+            .title("Test")
+            .content("Test")
+            .build();
+
+        // when
+        todo.complete();
+
+        // then
+        assertTrue(todo.getIsCompleted());
+
+    }
+}

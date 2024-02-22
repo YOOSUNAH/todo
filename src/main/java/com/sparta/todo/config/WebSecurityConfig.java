cab3ecf08835464efcca40f92a9293b2e6e863cb
YOOSUNAH <tjsdkyd4027@gmail.com>
2024-02-22 16:27:12 +0900

    Todo, User controller 테스트 구현 중

--- src/main/java/com/sparta/todo/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/todo/config/WebSecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -24,6 +26,11 @@ import java.util.List;
 public class WebSecurityConfig implements WebMvcConfigurer {
 
     private final AuthFilter authFilter;
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
---
+++ src/test/java/com/sparta/todo/controller/UserTodoControllerTest.java
@@ -0,0 +1,100 @@
+package com.sparta.todo.controller;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.todo.config.WebSecurityConfig;
+import com.sparta.todo.entity.User;
+import com.sparta.todo.entity.UserDetailsImpl;
+import com.sparta.todo.mvc.MockSpringSecurityFilter;
+import com.sparta.todo.service.TodoService;
+import com.sparta.todo.service.UserService;
+import java.security.Principal;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@WebMvcTest(
+    controllers = {UserController.class, TodoController.class},
+    // 제외할 filter
+    excludeFilters = {
+        @ComponentScan.Filter(
+            type = FilterType.ASSIGNABLE_TYPE,
+            classes = WebSecurityConfig.class  //  우리가 만든 WebSecurityConfig
+        )
+    }
+)
+class UserTodoControllerTest {
+
+    private MockMvc mvc;
+    private Principal mockPrincipal;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    UserService userService;
+
+    @MockBean
+    TodoService todoService;
+
+    @BeforeEach
+    public void setup() {
+        mvc = MockMvcBuilders.webAppContextSetup(context) // setup에 집어 넣기 위해서 context를 위에서 @Autowired 해서 받아왔다.
+            .apply(springSecurity(new MockSpringSecurityFilter())) // 우리가 만든 filter : MockSpringSecurityFilter
+            .build();
+    }
+
+
+    private void mockUserSetup() {
+        // Mock 테스트 유져 생성
+        String username = "sollertia4351";
+        String password = "robbie1234";
+        User testUser = new User(username, password);
+        UserDetailsImpl testUserDetails = new UserDetailsImpl(testUser);
+        mockPrincipal = new UsernamePasswordAuthenticationToken(testUserDetails, "", testUserDetails.getAuthorities());
+    }
+
+
+    @Test
+    @DisplayName("로그인")
+    void login() throws Exception {
+        mvc.perform(post("/api/v1/user/login"))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("회원 가입 요청")
+    void getTodoList() {
+    }
+
+    @Test
+    void getTodoById() {
+    }
+
+    @Test
+    void putTodo() {
+    }
+
+    @Test
+    void completeTodo() {
+    }
+
+    @Test
+    void deleteTodo() {
+    }
+}

f5c327b3e0039c64240f5048c8697fb7a0ae5338
YOOSUNAH <tjsdkyd4027@gmail.com>
2024-02-22 11:42:08 +0900

    :sparkles: Todo entity 테스트 코드 구현

--- src/test/java/com/sparta/todo/entity/TodoTest.java
+++ src/test/java/com/sparta/todo/entity/TodoTest.java
@@ -4,7 +4,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@SpringBootTest
+@ActiveProfiles("test")
 class TodoTest {
 
     @Test
